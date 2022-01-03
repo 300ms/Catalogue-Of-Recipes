745e596d550eecc37e876baf7fee6673d9827b47
300ms <ilhansnmz44@gmail.com>
2022-01-04 00:18:19 +0300

    Add negative scenario for reducer as requested

--- src/Tests/Reducer Tests/singleRecipe.test.js
+++ src/Tests/Reducer Tests/singleRecipe.test.js
@@ -2,6 +2,7 @@ import recipeDetails from '../../Reducers/RecipeDetails';
 
 import {
   FETCH_MEAL_SUCCESS,
+  FETCH_PRODUCTS_ERROR,
 } from '../../Constants/Types';
 
 const initialState = {
@@ -15,5 +16,13 @@ describe('Empty recipes', () => {
   it('Have only 1 recipe', () => {
     expect(recipeDetails(initialState, { type: FETCH_MEAL_SUCCESS, details }))
       .toEqual({ ...initialState, details });
+  });
+});
+
+describe('Fetch Failure', () => {
+  const errorMessage = 'Fetch Failure';
+  it('Action type is FETCH_MEAL_FAILURE', () => {
+    expect(recipeDetails(initialState, { type: FETCH_PRODUCTS_ERROR, error: errorMessage }))
+      .toEqual({ ...initialState, error: errorMessage });
   });
 });
